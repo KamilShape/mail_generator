aa541c1bb2e7fdd56603ad0d311091343f2b2160
KamilShape <kamilshape@gmail.com>
2022-11-23 19:30:49 +0100

    add some RWD styles

--- data.js
+++ data.js
@@ -1,1 +1,10 @@
-const welcomes = ['Hi!', 'Welcome,', 'Godd morning,', 'Dear']
+const welcomes = ['Hi!', 'Welcome,', 'Godd morning,', 'Dear'];
+
+let messageArea = document.getElementsByClassName('message');
+
+
+(function() {
+    messageArea.value = 'HhahAHAhAhAha'
+    messageArea.value += ' HhahAHAhAhAha'
+    console.log(messageArea)
+})();

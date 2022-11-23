72dcce42058356a70d799706a8cd0d981323bb5a
KamilShape <kamilshape@gmail.com>
2022-11-23 20:46:32 +0100

    add makeMessages function

--- data.js
+++ data.js
@@ -1,10 +1,34 @@
-const welcomes = ['Hi!', 'Welcome,', 'Godd morning,', 'Dear'];
+const welcomes = ['Hi!', 'Welcome,', 'Godd morning,', 'Dear,'];
+const content = ['Hi!', 'Welcome,', 'Godd morning,', 'Dear'];
+const regards = ['Good luck!', 'Best Regards!', 'Bye!', 'Goodbye!'];
 
-let messageArea = document.getElementsByClassName('message');
+const welcomes_wrapper = document.querySelector('.welcomes')
+const content_wrapper = document.querySelector('.content')
+const regards_wrapper = document.querySelector('.regards')
+
+const makeMessages = function(wrapper, array) {
+    array.forEach(el => {
+        let message = document.createElement('div')
+        message.classList.add('messages_message')
+        message.innerText = el
+        wrapper.appendChild(message)
+    })
+}
+
+makeMessages(welcomes_wrapper, welcomes)
+makeMessages(content_wrapper, content)
+makeMessages(regards_wrapper, regards)
+
+let messageArea = document.querySelector('.message');
+
+
+
+
 
 
 (function() {
     messageArea.value = 'HhahAHAhAhAha'
     messageArea.value += ' HhahAHAhAhAha'
     console.log(messageArea)
+    console.log(wrappers)
 })();
